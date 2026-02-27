73677399341deea436b6d4528d8e9c1db0b35ec3
jf2021070309 <jaimeelias.tacna.2016@gmail.com>
2026-02-27 11:41:22 -0500

    test2

--- test.php
+++ test.php
@@ -1,6 +1,27 @@
 <?php
-require_once 'config/conexion.php';
 
-echo "<h2>Conexión exitosa</h2>";
-echo "Host: $host <br>";
-echo "DB: $db <br>";
+echo "<h1>PHP OK</h1>";
+
+echo "<h2>Variables Railway:</h2>";
+
+echo "MYSQLHOST: " . getenv('MYSQLHOST') . "<br>";
+echo "MYSQLPORT: " . getenv('MYSQLPORT') . "<br>";
+echo "MYSQLDATABASE: " . getenv('MYSQLDATABASE') . "<br>";
+echo "MYSQLUSER: " . getenv('MYSQLUSER') . "<br>";
+
+echo "<hr>";
+
+$host = getenv('MYSQLHOST');
+$port = getenv('MYSQLPORT');
+$db   = getenv('MYSQLDATABASE');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+
+$conn = new mysqli($host, $user, $pass, $db, (int)$port);
+
+if ($conn->connect_error) {
+    echo "<h2 style='color:red'>ERROR MYSQL:</h2>";
+    echo $conn->connect_error;
+} else {
+    echo "<h2 style='color:green'>MYSQL CONECTADO OK</h2>";
+}

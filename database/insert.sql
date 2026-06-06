ec3ada4fdbd27e1705aadd8e4d5446d0f2e67d0a
Andree Flores Melendez <af2017057494@virtual.upt>
2026-06-06 14:02:45 -0600

    v246

--- database/insert.sql
+++ database/insert.sql
@@ -15,12 +15,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 -- Volcando datos para la tabla hotel_db.anticipos: ~2 rows (aproximadamente)
-INSERT INTO `anticipos` (`id`, `stay_id`, `monto`, `moneda`, `monto_pen`, `tc_aplicado`, `tipo_pago`, `recibo`, `fecha`, `aplicado`, `observacion`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `anticipos` (`id`, `stay_id`, `monto`, `moneda`, `monto_pen`, `tc_aplicado`, `tipo_pago`, `recibo`, `fecha`, `aplicado`, `observacion`, `usuario_id`, `created_at`) VALUES
 	(1, 1, 300.00, 'PEN', 300.00, 1.0000, 'SOLES EFECTIVO', '', '2026-05-31', 0, NULL, 4, '2026-05-31 17:20:25'),
 	(2, 2, 500.00, 'PEN', 500.00, 1.0000, 'SOLES EFECTIVO', '', '2026-05-31', 0, NULL, 4, '2026-05-31 18:55:39');
 
 -- Volcando datos para la tabla hotel_db.auditoria: ~43 rows (aproximadamente)
-INSERT INTO `auditoria` (`id`, `usuario_id`, `accion`, `modulo`, `detalle`, `fecha_hora`, `ip`) VALUES
+INSERT IGNORE INTO `auditoria` (`id`, `usuario_id`, `accion`, `modulo`, `detalle`, `fecha_hora`, `ip`) VALUES
 	(1, 4, 'GUARDAR_ROOMING_V2', 'ROOMING', 'Guardó/actualizó registros en la grilla plana Rooming V2', '2026-05-31 17:20:25', '::1'),
 	(2, 4, 'REGISTRAR_DESAYUNO', 'COMIDA', 'Consolidó el padrón de desayunos para la fecha: 2026-05-31', '2026-05-31 17:57:57', '::1'),
 	(3, 4, 'LIMPIEZA_ESTADO', 'LIMPIEZA', 'Marcó como LIMPIA la Habitación #202', '2026-05-31 18:31:14', '::1'),
@@ -66,16 +66,16 @@ INSERT INTO `auditoria` (`id`, `usuario_id`, `accion`, `modulo`, `detalle`, `fec
 	(43, 4, 'INICIO_SESION', 'SEGURIDAD', 'Inicio de sesión exitoso (Trabajador)', '2026-06-06 17:20:44', '::1');
 
 -- Volcando datos para la tabla hotel_db.caja_chica: ~2 rows (aproximadamente)
-INSERT INTO `caja_chica` (`id`, `nombre`, `saldo_inicial`, `saldo_final`, `fecha_apertura`, `fecha_cierre`, `estado`, `usuario_apertura`, `usuario_cierre`, `created_at`) VALUES
+INSERT IGNORE INTO `caja_chica` (`id`, `nombre`, `saldo_inicial`, `saldo_final`, `fecha_apertura`, `fecha_cierre`, `estado`, `usuario_apertura`, `usuario_cierre`, `created_at`) VALUES
 	(1, 'DDD', 100.00, 50.00, '2026-05-31', '2026-06-03', 'cerrada', 4, 4, '2026-05-31 20:56:43'),
 	(2, 'FONDO FIJO S/ 100 - 03/06/2026', 100.00, NULL, '2026-06-03', NULL, 'abierta', 4, NULL, '2026-06-03 17:34:39');
 
 -- Volcando datos para la tabla hotel_db.caja_chica_movimientos: ~1 rows (aproximadamente)
-INSERT INTO `caja_chica_movimientos` (`id`, `caja_id`, `tipo`, `monto`, `categoria_id`, `rubro`, `documento`, `fecha`, `observacion`, `usuario_id`, `anulado`, `motivo_anulacion`, `created_at`) VALUES
+INSERT IGNORE INTO `caja_chica_movimientos` (`id`, `caja_id`, `tipo`, `monto`, `categoria_id`, `rubro`, `documento`, `fecha`, `observacion`, `usuario_id`, `anulado`, `motivo_anulacion`, `created_at`) VALUES
 	(1, 1, 'egreso', 50.00, NULL, 'DD', 'DD', '2026-06-03', 'PAN', 4, 0, NULL, '2026-06-03 17:34:22');
 
 -- Volcando datos para la tabla hotel_db.clientes: ~6 rows (aproximadamente)
-INSERT INTO `clientes` (`id`, `tipo_cliente`, `documento_tipo`, `documento_num`, `ruc`, `empresa`, `nombre_razon_social`, `celular`, `email`, `nacionalidad`, `pais_origen`, `ciudad`, `created_at`) VALUES
+INSERT IGNORE INTO `clientes` (`id`, `tipo_cliente`, `documento_tipo`, `documento_num`, `ruc`, `empresa`, `nombre_razon_social`, `celular`, `email`, `nacionalidad`, `pais_origen`, `ciudad`, `created_at`) VALUES
 	(1, 'NATURAL', 'DNI', '76032957', NULL, NULL, 'Andree Flores Melendez', NULL, NULL, 'Peruana', NULL, 'Tacna', '2026-05-31 17:20:25'),
 	(2, 'NATURAL', 'DNI', '76329578', '', '', 'Jaime Flores', '957084266', 'jf2017057494@virtual.upt.pe', 'Peruana', NULL, 'Lima', '2026-05-31 18:55:39'),
 	(3, 'NATURAL', 'DNI', '', NULL, NULL, 'juan', NULL, NULL, 'Peruana', NULL, NULL, '2026-06-05 19:38:50'),
@@ -86,18 +86,18 @@ INSERT INTO `clientes` (`id`, `tipo_cliente`, `documento_tipo`, `documento_num`,
 -- Volcando datos para la tabla hotel_db.configuracion: ~0 rows (aproximadamente)
 
 -- Volcando datos para la tabla hotel_db.desayunos: ~2 rows (aproximadamente)
-INSERT INTO `desayunos` (`id`, `fecha`, `pax_calculado`, `pax_ajustado`, `observacion`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `desayunos` (`id`, `fecha`, `pax_calculado`, `pax_ajustado`, `observacion`, `usuario_id`, `created_at`) VALUES
 	(1, '2026-05-31', 1, 1, '', 4, '2026-05-31 17:57:57'),
 	(2, '2026-06-01', 2, 2, '', 4, '2026-05-31 19:19:59');
 
 -- Volcando datos para la tabla hotel_db.desayunos_detalle: ~3 rows (aproximadamente)
-INSERT INTO `desayunos_detalle` (`id`, `desayuno_id`, `stay_id`, `pax`, `incluye_desayuno`) VALUES
+INSERT IGNORE INTO `desayunos_detalle` (`id`, `desayuno_id`, `stay_id`, `pax`, `incluye_desayuno`) VALUES
 	(1, 1, 1, 1, 1),
 	(4, 2, 1, 1, 1),
 	(5, 2, 2, 1, 1);
 
 -- Volcando datos para la tabla hotel_db.finanzas_categorias: ~28 rows (aproximadamente)
-INSERT INTO `finanzas_categorias` (`id`, `modulo`, `tipo`, `nombre`, `orden`, `activo`) VALUES
+INSERT IGNORE INTO `finanzas_categorias` (`id`, `modulo`, `tipo`, `nombre`, `orden`, `activo`) VALUES
 	(1, 'Flujo', 'Ingreso', 'DEPOS/TRANS.', 1, 1),
 	(2, 'Flujo', 'Ingreso', 'YAPE O PLIN', 2, 1),
 	(3, 'Flujo', 'Ingreso', 'POS DOLARES', 3, 1),
@@ -128,25 +128,25 @@ INSERT INTO `finanzas_categorias` (`id`, `modulo`, `tipo`, `nombre`, `orden`, `a
 	(28, 'C.Chica', 'Ingreso', 'REPOSICIÓN CAJA', 1, 1);
 
 -- Volcando datos para la tabla hotel_db.flujo_caja: ~1 rows (aproximadamente)
-INSERT INTO `flujo_caja` (`id`, `fecha`, `turno`, `estado`, `nota_entrega`, `usuario_id`, `created_at`, `updated_at`) VALUES
+INSERT IGNORE INTO `flujo_caja` (`id`, `fecha`, `turno`, `estado`, `nota_entrega`, `usuario_id`, `created_at`, `updated_at`) VALUES
 	(1, '2026-05-31', 'MAÑANA', 'borrador', 'Apertura automática desde Rooming V2', 4, '2026-05-31 17:20:25', '2026-05-31 17:20:25');
 
 -- Volcando datos para la tabla hotel_db.flujo_caja_movimientos: ~4 rows (aproximadamente)
-INSERT INTO `flujo_caja_movimientos` (`id`, `flujo_id`, `categoria_id`, `stay_id`, `tipo`, `monto`, `moneda`, `medio_pago`, `documento`, `observacion`, `vuelto`, `created_at`) VALUES
+INSERT IGNORE INTO `flujo_caja_movimientos` (`id`, `flujo_id`, `categoria_id`, `stay_id`, `tipo`, `monto`, `moneda`, `medio_pago`, `documento`, `observacion`, `vuelto`, `created_at`) VALUES
 	(1, 1, 7, 1, 'Ingreso', 300.00, 'PEN', 'EFECTIVO', NULL, 'HOSPEDAJE: Andree Flores Melendez - Registro #1 (Hab #202)', 0.00, '2026-05-31 17:20:25'),
 	(2, 1, 7, 2, 'Ingreso', 500.00, 'PEN', 'EFECTIVO', NULL, 'HOSPEDAJE: Jaime Flores - Registro #2 (Hab #205)', 0.00, '2026-05-31 18:55:39'),
 	(3, 1, 14, NULL, 'Egreso', 100.00, 'PEN', 'EFECTIVO', NULL, 'Apertura Ciclo #1: DDD', 0.00, '2026-05-31 20:56:43'),
 	(4, 1, 14, NULL, 'Egreso', 100.00, 'PEN', 'EFECTIVO', NULL, 'Apertura Ciclo #2: FONDO FIJO S/ 100 - 03/06/2026', 0.00, '2026-06-03 17:34:39');
 
 -- Volcando datos para la tabla hotel_db.gastos_yape: ~4 rows (aproximadamente)
-INSERT INTO `gastos_yape` (`id`, `fecha`, `turno`, `yape_recibido`, `total_gastado`, `vuelto`, `observacion`, `estado`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `gastos_yape` (`id`, `fecha`, `turno`, `yape_recibido`, `total_gastado`, `vuelto`, `observacion`, `estado`, `usuario_id`, `created_at`) VALUES
 	(1, '2026-05-31', 'MAÑANA', 500.00, 175.00, 325.00, '', 'borrador', 4, '2026-05-31 20:28:45'),
 	(2, '2026-05-31', 'TARDE', 0.00, 0.00, 0.00, '', 'borrador', 4, '2026-05-31 20:28:45'),
 	(3, '2026-06-02', 'MAÑANA', 0.00, 0.00, 0.00, '', 'borrador', 4, '2026-06-02 17:21:47'),
 	(4, '2026-06-02', 'TARDE', 0.00, 0.00, 0.00, '', 'borrador', 4, '2026-06-02 17:21:47');
 
 -- Volcando datos para la tabla hotel_db.gastos_yape_detalle: ~6 rows (aproximadamente)
-INSERT INTO `gastos_yape_detalle` (`id`, `gasto_yape_id`, `categoria_id`, `rubro`, `monto`, `observacion`, `documento`, `created_at`) VALUES
+INSERT IGNORE INTO `gastos_yape_detalle` (`id`, `gasto_yape_id`, `categoria_id`, `rubro`, `monto`, `observacion`, `documento`, `created_at`) VALUES
 	(1, 1, NULL, 'MERCADO', 100.00, '', '', '2026-05-31 20:32:08'),
 	(2, 1, NULL, 'MOVILIDAD', 5.00, '', '', '2026-05-31 20:32:08'),
 	(3, 1, NULL, 'CAFETERÍA/VEA', 10.00, '', '', '2026-05-31 20:32:08'),
@@ -155,7 +155,7 @@ INSERT INTO `gastos_yape_detalle` (`id`, `gasto_yape_id`, `categoria_id`, `rubro
 	(6, 1, NULL, 'OTROS', 10.00, '', '', '2026-05-31 20:32:08');
 
 -- Volcando datos para la tabla hotel_db.habitaciones: ~25 rows (aproximadamente)
-INSERT INTO `habitaciones` (`id`, `numero`, `tipo`, `piso`, `estado`, `precio_base`, `descripcion`, `activa`) VALUES
+INSERT IGNORE INTO `habitaciones` (`id`, `numero`, `tipo`, `piso`, `estado`, `precio_base`, `descripcion`, `activa`) VALUES
 	(1, '201', 'TRIPLE FAMILIAR', 2, 'ocupado', 200.00, NULL, 1),
 	(2, '202', 'EJECUTIVA SUPERIOR', 2, 'libre', 150.00, NULL, 1),
 	(3, '203', 'DOBLE', 2, 'libre', 180.00, NULL, 1),
@@ -183,12 +183,12 @@ INSERT INTO `habitaciones` (`id`, `numero`, `tipo`, `piso`, `estado`, `precio_ba
 	(25, '605', 'PLATINIUM SUITE', 6, 'libre', 260.00, NULL, 1);
 
 -- Volcando datos para la tabla hotel_db.inventario_movimientos: ~2 rows (aproximadamente)
-INSERT INTO `inventario_movimientos` (`id`, `producto_id`, `tipo`, `cantidad`, `stock_antes`, `stock_despues`, `stay_id`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `inventario_movimientos` (`id`, `producto_id`, `tipo`, `cantidad`, `stock_antes`, `stock_despues`, `stay_id`, `usuario_id`, `created_at`) VALUES
 	(1, 4, 'RECARGA', 10, 10, 20, NULL, 1, '2026-06-01 18:01:11'),
 	(2, 4, 'RECARGA', 10, 20, 30, NULL, 4, '2026-06-01 18:08:07');
 
 -- Volcando datos para la tabla hotel_db.inventario_productos: ~9 rows (aproximadamente)
-INSERT INTO `inventario_productos` (`id`, `nombre`, `categoria`, `refrigeradora`, `precio_venta`, `stock_actual`, `activo`) VALUES
+INSERT IGNORE INTO `inventario_productos` (`id`, `nombre`, `categoria`, `refrigeradora`, `precio_venta`, `stock_actual`, `activo`) VALUES
 	(1, 'COCA COLA', 'BEBIDA', 1, 7.00, 10, 1),
 	(2, 'INCA COLA', 'BEBIDA', 1, 7.00, 10, 1),
 	(3, 'AGUA SAN MATEO', 'BEBIDA', 1, 5.00, 10, 1),
@@ -200,7 +200,7 @@ INSERT INTO `inventario_productos` (`id`, `nombre`, `categoria`, `refrigeradora`
 	(9, 'VINO AZUL', 'VINO', 2, 35.00, 10, 1);
 
 -- Volcando datos para la tabla hotel_db.limpieza_registros: ~5 rows (aproximadamente)
-INSERT INTO `limpieza_registros` (`id`, `fecha`, `habitacion_id`, `tipo_limpieza`, `prioridad`, `estado`, `hora_inicio`, `hora_fin`, `observacion`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `limpieza_registros` (`id`, `fecha`, `habitacion_id`, `tipo_limpieza`, `prioridad`, `estado`, `hora_inicio`, `hora_fin`, `observacion`, `usuario_id`, `created_at`) VALUES
 	(1, '2026-05-31', 2, 'salida', 'alta', 'lista', NULL, '2026-05-31 13:31:14', NULL, 4, '2026-05-31 17:20:57'),
 	(2, '2026-06-01', 2, 'salida', 'normal', 'pendiente', NULL, NULL, NULL, 4, '2026-06-01 15:54:46'),
 	(3, '2026-06-01', 5, 'salida', 'normal', 'pendiente', NULL, NULL, NULL, 4, '2026-06-01 15:54:46'),
@@ -210,7 +210,7 @@ INSERT INTO `limpieza_registros` (`id`, `fecha`, `habitacion_id`, `tipo_limpieza
 -- Volcando datos para la tabla hotel_db.rooming_consumos: ~0 rows (aproximadamente)
 
 -- Volcando datos para la tabla hotel_db.rooming_pax: ~6 rows (aproximadamente)
-INSERT INTO `rooming_pax` (`id`, `stay_id`, `cliente_id`, `es_titular_acompanante`, `created_at`, `vip`) VALUES
+INSERT IGNORE INTO `rooming_pax` (`id`, `stay_id`, `cliente_id`, `es_titular_acompanante`, `created_at`, `vip`) VALUES
 	(1, 1, 1, 1, '2026-05-31 17:20:25', 0),
 	(2, 2, 2, 1, '2026-05-31 18:55:39', 0),
 	(3, 3, 3, 1, '2026-06-05 19:38:50', 0),
@@ -219,7 +219,7 @@ INSERT INTO `rooming_pax` (`id`, `stay_id`, `cliente_id`, `es_titular_acompanant
 	(6, 6, 9, 1, '2026-06-05 21:25:20', 0);
 
 -- Volcando datos para la tabla hotel_db.rooming_stays: ~6 rows (aproximadamente)
-INSERT INTO `rooming_stays` (`id`, `operador`, `fecha_registro`, `fecha_checkin_real`, `fecha_checkout`, `medio_reserva`, `habitacion_id`, `tipo_hab_declarado`, `pax_total`, `total_pago`, `moneda_pago`, `monto_original`, `tc_aplicado`, `recargo_tarjeta`, `metodo_pago`, `tipo_comprobante`, `num_comprobante`, `cobrador`, `procedencia`, `carro`, `estado`, `estado_pago`, `total_cobrado`, `total_cobrado_orig`, `checkin_realizado`, `observaciones`, `usuario_id`, `created_at`, `updated_at`, `cliente_titular_id`) VALUES
+INSERT IGNORE INTO `rooming_stays` (`id`, `operador`, `fecha_registro`, `fecha_checkin_real`, `fecha_checkout`, `medio_reserva`, `habitacion_id`, `tipo_hab_declarado`, `pax_total`, `total_pago`, `moneda_pago`, `monto_original`, `tc_aplicado`, `recargo_tarjeta`, `metodo_pago`, `tipo_comprobante`, `num_comprobante`, `cobrador`, `procedencia`, `carro`, `estado`, `estado_pago`, `total_cobrado`, `total_cobrado_orig`, `checkin_realizado`, `observaciones`, `usuario_id`, `created_at`, `updated_at`, `cliente_titular_id`) VALUES
 	(1, 'Roy', '2026-05-31', '2026-05-31 12:20:00', '2026-06-04', 'DIRECTO', 2, 'EJECUTIVA SUPERIOR', 1, 300.00, 'PEN', 300.00, NULL, 0.00, 'SOLES EFECTIVO', '', '', 'Roy', NULL, 'NO', 'activo', 'pagado', 300.00, 300.00, 1, 'sin tv', 4, '2026-05-31 17:20:25', '2026-05-31 18:38:21', 1),
 	(2, 'Roy', '2026-05-31', '2026-05-31 13:55:00', '2026-06-02', 'DIRECTO', 5, 'PLATINIUM SUITE', 1, 500.00, 'PEN', 500.00, NULL, 0.00, 'SOLES EFECTIVO', '', '', 'Roy', NULL, 'NO', 'activo', 'pagado', 500.00, 500.00, 1, '', 4, '2026-05-31 18:55:39', '2026-06-02 17:52:39', 2),
 	(3, 'Roy', '2026-06-05', NULL, '2026-06-08', 'DIRECTO', 1, 'RESERVA', 1, 0.00, 'PEN', NULL, NULL, 0.00, 'EFECTIVO', '', NULL, 'Roy', NULL, NULL, 'activo', 'pendiente', 0.00, 0.00, 0, 'sin television', 4, '2026-06-05 19:38:50', '2026-06-05 19:39:00', 3),
@@ -228,18 +228,18 @@ INSERT INTO `rooming_stays` (`id`, `operador`, `fecha_registro`, `fecha_checkin_
 	(6, 'Roy', '2026-06-05', NULL, '2026-06-06', 'DIRECTO', 7, 'RESERVA', 1, 0.00, 'PEN', NULL, NULL, 0.00, 'EFECTIVO', '', NULL, 'Roy', NULL, NULL, 'activo', 'pendiente', 0.00, 0.00, 1, 'con piscina', 4, '2026-06-05 21:25:20', '2026-06-05 21:25:39', 9);
 
 -- Volcando datos para la tabla hotel_db.rooming_stays_historial_fechas: ~3 rows (aproximadamente)
-INSERT INTO `rooming_stays_historial_fechas` (`id`, `stay_id`, `fecha_checkout_pasada`, `motivo`, `usuario_id`, `created_at`) VALUES
+INSERT IGNORE INTO `rooming_stays_historial_fechas` (`id`, `stay_id`, `fecha_checkout_pasada`, `motivo`, `usuario_id`, `created_at`) VALUES
 	(1, 1, '2026-05-31', 'Ampliación de estadía desde grilla V2', 4, '2026-05-31 18:36:48'),
 	(2, 1, '2026-06-03', 'Ampliación de estadía desde grilla V2', 4, '2026-05-31 18:38:21'),
 	(3, 2, '2026-05-31', 'Ampliación de estadía desde grilla V2', 4, '2026-05-31 18:55:48');
 
 -- Volcando datos para la tabla hotel_db.tipos_cambio: ~2 rows (aproximadamente)
-INSERT INTO `tipos_cambio` (`id`, `moneda_origen`, `moneda_destino`, `factor`, `fecha`, `created_at`) VALUES
+INSERT IGNORE INTO `tipos_cambio` (`id`, `moneda_origen`, `moneda_destino`, `factor`, `fecha`, `created_at`) VALUES
 	(1, 'USD', 'PEN', 3.7000, '2026-05-31', '2026-05-31 17:10:46'),
 	(2, 'CLP', 'PEN', 277.0000, '2026-05-31', '2026-05-31 17:10:46');
 
 -- Volcando datos para la tabla hotel_db.usuarios: ~5 rows (aproximadamente)
-INSERT INTO `usuarios` (`id`, `usuario`, `password`, `rol`, `nombre`, `estado`, `created_at`) VALUES
+INSERT IGNORE INTO `usuarios` (`id`, `usuario`, `password`, `rol`, `nombre`, `estado`, `created_at`) VALUES
 	(1, 'admin', '$2y$10$Y/GanjL/y/VpLwNpr2gNgua41aXNlmDuTxeScRsvw9Vh8zIHIt0uS', 'admin', 'Administrador', 1, '2026-05-31 17:10:46'),
 	(2, 'kari', '$2y$10$Y/GanjL/y/VpLwNpr2gNgua41aXNlmDuTxeScRsvw9Vh8zIHIt0uS', 'limpieza', 'Kari', 1, '2026-05-31 17:10:46'),
 	(3, 'jessica', '$2y$10$Y/GanjL/y/VpLwNpr2gNgua41aXNlmDuTxeScRsvw9Vh8zIHIt0uS', 'cajera', 'Jessica', 1, '2026-05-31 17:10:46'),

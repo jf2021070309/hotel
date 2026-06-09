075e24228a6ea1a7e42ac1b3ed334cca9484fe01
Andree Flores Melendez <af2017057494@virtual.upt>
2026-06-09 09:32:42 -0600

    v278

--- app/Views/clientes/v2.php
+++ app/Views/clientes/v2.php
@@ -84,16 +84,16 @@ include $_projectRoot . '/app/Views/layouts/head.php';
           <table class="table table-bordered table-hover mb-0 align-middle table-mensual">
             <thead class="table-dark text-white text-uppercase text-center" style="font-size: 10px; letter-spacing: 0.5px;">
               <tr>
-                <th class="sticky-col" style="padding: 12px 16px; min-width: 60px; width: 60px; z-index: 12 !important;"><i class="bi bi-trash"></i></th>
-                <th style="padding: 12px 16px; min-width: 600px; width: 600px;">NOMBRE</th>
-                <th style="padding: 12px 16px; min-width: 200px; width: 200px;">DNI</th>
-                <th style="padding: 12px 16px; min-width: 200px; width: 200px;">NACIONALIDAD</th>
-                <th style="padding: 12px 16px; min-width: 200px; width: 200px;">CIUDAD</th>
-                <th style="padding: 12px 16px; min-width: 200px; width: 200px;">CELULAR</th>
-                <th style="padding: 12px 16px; min-width: 400px; width: 400px;">EMAIL</th>
-                <th style="padding: 12px 16px; min-width: 200px; width: 200px;">RUC</th>
-                <th style="padding: 12px 16px; min-width: 600px; width: 600px;">EMPRESA</th>
-                <th style="padding: 12px 16px; min-width: 250px; width: 250px;">ACCIONES</th>
+                <th class="sticky-col" style="padding: 12px 16px; min-width: 50px; width: 50px; z-index: 12 !important;"><i class="bi bi-trash"></i></th>
+                <th style="padding: 12px 16px; min-width: 350px; width: 350px;">NOMBRE</th>
+                <th style="padding: 12px 16px; min-width: 120px; width: 120px;">DNI</th>
+                <th style="padding: 12px 16px; min-width: 150px; width: 150px;">NACIONALIDAD</th>
+                <th style="padding: 12px 16px; min-width: 150px; width: 150px;">CIUDAD</th>
+                <th style="padding: 12px 16px; min-width: 120px; width: 120px;">CELULAR</th>
+                <th style="padding: 12px 16px; min-width: 250px; width: 250px;">EMAIL</th>
+                <th style="padding: 12px 16px; min-width: 150px; width: 150px;">RUC</th>
+                <th style="padding: 12px 16px; min-width: 350px; width: 350px;">EMPRESA</th>
+                <th style="padding: 12px 16px; min-width: 160px; width: 160px;">ACCIONES</th>
               </tr>
             </thead>
             
@@ -237,8 +237,8 @@ include $_projectRoot . '/app/Views/layouts/head.php';
   }
 
   .table-mensual {
-    width: 3000px !important;
-    min-width: 3000px !important;
+    width: 1800px !important;
+    min-width: 1800px !important;
     max-width: none !important;
     font-size: 11px;
     border-collapse: separate;
